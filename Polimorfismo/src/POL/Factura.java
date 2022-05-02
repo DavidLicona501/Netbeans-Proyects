c9cd1d45a84be77b019dcdc8f6c5cc4fe6d25e36
DavidLicona501 <104457925+DavidLicona501@users.noreply.github.com>
2022-05-02 12:52:46 -0600

    Add files via upload
    Nueva actualización

---
+++ Polimorfismo/src/POL/Factura.java
@@ -0,0 +1,67 @@
+/*
+ 
+ */
+package POL;
+
+/**
+ *
+ * @author invitado
+ */
+public class Factura {
+  private String numeroPieza;
+  private String descripcionesPieza;
+  private int cantidad;
+  private double precioPorArticulo;
+ 
+  //Se crea un constructor 
+  public Factura(String pieza, String descripcion, int cuenta, double precio){
+      numeroPieza=pieza;
+      descripcionesPieza=descripcion;
+      
+      // se crean los metodos 
+      establecerCantidad(cuenta);
+      establecerPrecioPorArticulo(precio);
+      
+  }
+  // se desarrollan metodos get/set para el resto de las variables 
+  public void establecerNumeroPieza(String pieza){
+      numeroPieza=pieza;
+  }
+  public String obtenerNumeroPieza(){
+      return numeroPieza;
+  }
+  public void establecerDescripcionPieza(String descripcion){
+      descripcionesPieza=descripcion;
+  }
+  public String obtenerDescripcionPieza(){
+      return descripcionesPieza;
+  }
+  public void establecerCantidad(int cuenta){
+      if(cuenta>=0)
+          cantidad=cuenta;
+      else
+          throw new IllegalArgumentException("La cantidad debe ser mayor o igual a cero");
+  }
+  public int obtenerCantidad(){
+      return cantidad;
+  }
+  public void establecerPrecioPorArticulo(double precio){
+      if(precio>=0.00)
+            precioPorArticulo=precio;
+      else
+          throw new IllegalArgumentException("El precio por articulo debe ser mayor o igual a cero");
+  }
+  public double obtenerPrecioPorArticulo(){
+      return precioPorArticulo;
+  }
+  @Override
+  public String toString(){
+   return String.format("&s:\n&s:%s(%S)\n%s:  %d \n%s: $%,.2f","factura","numero de pieza",obtenerNumeroPieza(),obtenerDescripcionPieza(),"Cantidad",obtenerCantidad(),"Precio por articulo",obtenerPrecioPorArticulo());   
+  }
+  @Override
+  public double obtenerMontoPago(){
+      // Se calcula el costo total
+      return obtenerCantidad()*obtenerPrecioPorArticulo();
+      
+  }
+}
